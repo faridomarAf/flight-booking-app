93691f4af97dd6effb5ed90ec3d014dcca716fde
Farid Omar <fariedomar43@gmail.com>
2025-02-23 13:34:48 +0530

    modify airplane-middleware.js to handle error with AppError class

--- backend/src/middlewares/airplane-middleware.js
+++ backend/src/middlewares/airplane-middleware.js
@@ -1,10 +1,13 @@
 const {StatusCodes} = require('http-status-codes');
 const {ErrorResponse} = require('../utils/common');
+const { AppError } = require('../utils');
 
 const validateCreateRequest = async (req, res, next)=>{
     if(!req.body.modelNumber){
         ErrorResponse.message =  "Something went wrong while creating airplane!";
-        ErrorResponse.error = {explanation: "modelNumber not found in the correct format"};
+
+        //to keep handling error more consistently in explanation array
+        ErrorResponse.error = new AppError(["modelNumber not found in the correct format"],StatusCodes.BAD_REQUEST);
         return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
     }
 
